23d7f53abf506afafd2928de45fc62bdd5a57827
Raghavendra Achari <raghav.achari.l@gmail.com>
2022-09-11 15:57:39 +0530

    fixed index page problem
    before this, the index page was not showing up.
    after this fix index page is shown properly when user goes to homepage.

--- src/main/java/com/raghav/atom/controller/SinglePageAppController.java
+++ src/main/java/com/raghav/atom/controller/SinglePageAppController.java
@@ -2,14 +2,16 @@ package com.raghav.atom.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class SinglePageAppController {
-    @RequestMapping(value = {"/","/photography/**","/art/**"})
-    public String forward(){
-        return "index.html";
+    @RequestMapping(value = {"/","/photography/**","/art/**"}, method = RequestMethod.GET)
+    public String index(){
+        return "forward:/index.html";
     }
 }
--- src/main/java/com/raghav/atom/security/WebConfig.java
+++ src/main/java/com/raghav/atom/security/WebConfig.java
@@ -1,14 +1,19 @@
 package com.raghav.atom.security;
 
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
